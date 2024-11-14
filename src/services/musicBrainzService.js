b087a0ac968169dbf623b3a60cfb20a075d38da7
shoeb <shoebimran005@gmail.com>
2024-11-14 19:12:38 +0530

    API.

--- src/services/musicBrainzService.js
+++ src/services/musicBrainzService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchBandsByCity = async (city) => {
   try {
-    const response = await axios.get(`http://203.161.58.56/api/v1/bands/search?city=${city}`);
+    const response = await axios.get(`https://music-band-rails.onrender.com/api/v1/bands/search?city=${city}`);
     return response.data.bands || [];
   } catch (error) {
     console.error("Error fetching bands from backend:", error);
